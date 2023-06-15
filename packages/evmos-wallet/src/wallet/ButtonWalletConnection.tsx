b1c9a1bd7bd66d64fb2d7011fd098ffeacc20197
Milagros Paoletti <milagros.paoletti@gmail.com>
2023-06-15 10:43:14 +0200

    feat: disable Keplr if it is vesting app

--- packages/evmos-wallet/src/wallet/ButtonWalletConnection.tsx
+++ packages/evmos-wallet/src/wallet/ButtonWalletConnection.tsx
@@ -53,7 +53,7 @@ import {
 } from "tracker";
 // Components
 import { Button } from "ui-helpers";
-
+import { useRouter } from "next/router";
 export const ButtonWalletConnection = ({
   walletExtension,
   dispatch,
@@ -167,6 +167,8 @@ export const ButtonWalletConnection = ({
     }
     SaveWalletToLocalStorage(walletExtension.evmosAddressEthFormat);
   }, [walletExtension]);
+
+  const router = useRouter();
 
   return walletExtension.active === true ? (
     <>
@@ -298,6 +300,7 @@ export const ButtonWalletConnection = ({
                   });
                 }
               }}
+              disabled={router?.basePath === "/vesting"}
             >
               <ContentModalConnect>
                 <>
