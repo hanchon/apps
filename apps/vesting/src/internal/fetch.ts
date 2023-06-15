368eddba15d68ebbc7a1a034c004f6f3c76f3971
Milagros Paoletti <milagros.paoletti@gmail.com>
2023-06-15 14:48:37 +0200

    use account again

--- apps/vesting/src/internal/fetch.ts
+++ apps/vesting/src/internal/fetch.ts
@@ -2,7 +2,7 @@
 // SPDX-License-Identifier:ENCL-1.0(https://github.com/evmos/apps/blob/main/LICENSE)
 
 // import { EVMOS_BACKEND } from "evmos-wallet";
-import { isValidAccount } from "../components/vesting/helpers";
+// import { isValidAccount } from "../components/vesting/helpers";
 import { VestingResponse } from "./types";
 
 // TODO: change EVMOS_STAGING_BACKEND to EVMOS_BACKEND
@@ -13,15 +13,12 @@ export const getVesting = async (account: string | false) => {
     return "There is no vesting account linked to this address.";
   }
 
-  const isValid = isValidAccount(account);
-  if (!isValid || isValid === "undefined") {
-    return "Invalid account parameter.";
-  }
+  // const isValid = isValidAccount(account);
+  // if (!isValid || isValid === "undefined") {
+  //   return "There is no vesting account linked to this address.";
+  // }
   try {
-    const res = await fetch(
-      // eslint-disable-next-line no-secrets/no-secrets
-      `${EVMOS_STAGING_BACKEND}/v2/vesting/evmos1fwrmzh6kp2dh0wuevhzfsck0eeeqc54tpvkvc2`
-    );
+    const res = await fetch(`${EVMOS_STAGING_BACKEND}/v2/vesting/${account}`);
     return res.json() as Promise<VestingResponse>;
   } catch (error) {
     return "Error while getting vesting account info";
