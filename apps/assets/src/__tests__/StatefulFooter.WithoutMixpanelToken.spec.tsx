4795907179e443972a8f223676258b8acb659a56
Milagros Paoletti <40247040+miliPaoletti@users.noreply.github.com>
2023-06-30 17:18:21 +0200

    feat: mixpanel footer testing (#357)

---
+++ apps/assets/src/__tests__/StatefulFooter.WithoutMixpanelToken.spec.tsx
@@ -0,0 +1,36 @@
+import { test, describe, expect, vi } from "vitest";
+import { render } from "@testing-library/react";
+import { StatefulFooter } from "../StatefulFooter";
+import userEvent from "@testing-library/user-event";
+import mixpanel from "mixpanel-browser";
+import { MixpanelProvider } from "tracker";
+
+describe("Testing Footer - mixpanel is not setted", () => {
+  vi.mock("mixpanel-browser", async () => {
+    return {
+      default: {
+        init: vi.fn(),
+        track: vi.fn(),
+      },
+    };
+  });
+
+  test("should not call mixpanel after clicking on feedback", async () => {
+    const wrapperWithoutToken = ({ children }: { children: JSX.Element }) => {
+      return (
+        <MixpanelProvider token="" config={{ ip: false }}>
+          {children}
+        </MixpanelProvider>
+      );
+    };
+
+    const { getByText } = render(<StatefulFooter />, {
+      wrapper: wrapperWithoutToken,
+    });
+    const element = getByText("Feedback");
+    expect(element).toBeDefined();
+    await userEvent.click(element);
+    expect(mixpanel.init).toHaveBeenCalledOnce();
+    expect(mixpanel.track).not.toHaveBeenCalled();
+  });
+});
