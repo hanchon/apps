2a8333e4557902efdbe563ad2bf1c58e5da9b914
Mrsirdev <111534516+Mrsirdev@users.noreply.github.com>
2023-07-11 06:30:55 -0500

    chore: add i18n locales and config (#382)
    Co-authored-by: Milagros Paoletti <milagros.paoletti@gmail.com>
    Co-authored-by: Milagros Paoletti <40247040+miliPaoletti@users.noreply.github.com>
    Co-authored-by: Sandoche <sandoche@protonmail.com>

--- apps/mission/next.config.js
+++ apps/mission/next.config.js
@@ -1,6 +1,7 @@
 // Copyright Tharsis Labs Ltd.(Evmos)
 // SPDX-License-Identifier:ENCL-1.0(https://github.com/evmos/apps/blob/main/LICENSE)
-const { version } = require("./package.json");
+const { version } = require('./package.json');
+const { i18n } = require("./next-i18next.config");
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
@@ -10,10 +11,7 @@ const nextConfig = {
   reactStrictMode: true,
   basePath: "",
   swcMinify: true,
-  i18n: {
-    locales: ["en"],
-    defaultLocale: "en",
-  },
+  i18n,
   publicRuntimeConfig: {
     version,
   },
