e681df58c7847e1de5fde2d2b0572815be920b8d
Milagros Paoletti <milagros.paoletti@gmail.com>
2023-08-01 09:48:12 +0200

    add decimals to balance

--- apps/mission/src/components/mission/dappStore/accountBalance/AccountBalance.tsx
+++ apps/mission/src/components/mission/dappStore/accountBalance/AccountBalance.tsx
@@ -6,16 +6,31 @@ export const AccountBalance = () => {
   const { evmosPrice, totalEvmosAsset } = useAssetsTopBar();
 
   const totalEvmos = totalEvmosAsset.add(totalStaked);
-  const totalBalance = Number(convertFromAtto(totalEvmos)) + totalRewards;
+  // staked + evmos + rewards
+  let totalBalance = Number(convertFromAtto(totalEvmos)) + totalRewards;
 
-  const totalDollars = totalBalance * Number(evmosPrice);
+  const totalBalanceInDollars = totalBalance * Number(evmosPrice);
+
+  const drawTotalBalance = () => {
+    if (totalBalance < 1) {
+      return totalBalance.toFixed(2);
+    }
+    return totalBalance.toFixed(0);
+  };
+
+  const drawTotalBalanceInDollars = () => {
+    if (totalBalanceInDollars < 1) {
+      return totalBalanceInDollars.toFixed(2);
+    }
+    return totalBalanceInDollars.toFixed(0);
+  };
 
   return (
     <section className="text-[IBM]">
       <p className="text-2xl text-pearl ">Total Balance</p>
       <div className="flex items-center justify-center space-x-3">
         <span className="text-6xl font-bold text-white">
-          {wallet.active ? totalBalance.toFixed(2) : "-"}
+          {wallet.active ? drawTotalBalance() : "-"}
         </span>
         <span className="text-6xl font-bold text-white opacity-50">EVMOS</span>
         <button className="rounded bg-[#423D37] px-5 py-3 font-[IBM] text-sm font-bold text-pearl">
@@ -23,7 +38,7 @@ export const AccountBalance = () => {
         </button>
       </div>
       <p className="mt-4 text-xl text-white opacity-50">
-        $ {wallet.active ? totalDollars.toFixed(2) : "-"}
+        $ {wallet.active ? drawTotalBalanceInDollars() : "-"}
       </p>
     </section>
   );
