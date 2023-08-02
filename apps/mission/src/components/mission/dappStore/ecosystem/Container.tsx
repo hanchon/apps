d122375495454088eef61e7cc5ec6bafb666559a
Milagros Paoletti <milagros.paoletti@gmail.com>
2023-08-02 11:22:41 +0200

    update google form url

--- apps/mission/src/components/mission/dappStore/ecosystem/Container.tsx
+++ apps/mission/src/components/mission/dappStore/ecosystem/Container.tsx
@@ -1,9 +1,12 @@
+import Link from "next/link";
 import { Button } from "../Button";
 import { EcosystemCard } from "./Card";
 import { EcosystemProps, ecosystemData } from "./ecosystemData";
 
 const ecosystemPage =
   "https://altiplanic.notion.site/a188bd13dd114a88a7763fd2a8cc601e?v=403420ad21db41ce81f09b7e3f77e4e2";
+const googleFormUrl =
+  "https://docs.google.com/forms/d/e/1FAIpQLSc2O5HzjZMPbFD84O1513xJ0mggkSXihQy_a6IGskZq28l8yA/viewform";
 export const EcosystemContainer = () => {
   const handleViewAlldApps = () => {
     // redirect
@@ -21,7 +24,14 @@ export const EcosystemContainer = () => {
           <h1 className="text-2xl text-pearl">Featured Ecosystem dApps</h1>
           <h2 className="text-[#FFFFFFB2]">
             Interested in being a part of our ecosystem overview?
-            <span className="text-red"> Click here!</span>
+            <Link
+              className="ml-2 text-red"
+              href={googleFormUrl}
+              rel="noopener noreferrer"
+              target="_blank"
+            >
+              Click here!
+            </Link>
           </h2>
         </div>
         <Button text="View all dApps" handleOnClick={handleViewAlldApps} />
