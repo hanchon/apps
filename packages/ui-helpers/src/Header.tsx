64061c92086d6c40d75796fc7e6ddd001eaf2e96
Milagros Paoletti <milagros.paoletti@gmail.com>
2023-08-03 16:41:38 +0200

    add evmos price in header

--- packages/ui-helpers/src/Header.tsx
+++ packages/ui-helpers/src/Header.tsx
@@ -2,7 +2,7 @@
 // SPDX-License-Identifier:ENCL-1.0(https://github.com/evmos/apps/blob/main/LICENSE)
 
 import Link from "next/link";
-import { Logo } from "icons";
+import { EvmosRedIcon, Logo } from "icons";
 import { EVMOS_PAGE_URL } from "constants-helper";
 import { LaunchContainer } from "./launchPad/Container";
 export const Header = ({
@@ -26,9 +26,20 @@ export const Header = ({
           <span className="text-red text-2xl font-bold">Apps</span>
         </Link>
       </div>
-      <div className="flex items-center space-x-3">
-        <LaunchContainer />
-        {walletConnectionButton}
+      <div className="flex flex-col items-center space-y-3 md:flex-row md:space-x-16 md:space-y-0">
+        <div
+          className="font-sm text-pearl bg-darGray800 flex
+     items-center justify-center space-x-3 rounded-full px-10 py-2
+      font-bold"
+        >
+          <EvmosRedIcon width={"20"} height={"20"} />
+          {/* TODO: add evmos price */}
+          <span>-</span>
+        </div>
+        <div className="flex items-center space-x-3">
+          <LaunchContainer />
+          {walletConnectionButton}
+        </div>
       </div>
     </div>
   );
