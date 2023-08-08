72bfa49b44ccc780290793b11a5e53df326f03ab
Milagros Paoletti <milagros.paoletti@gmail.com>
2023-08-08 15:12:19 +0200

    add google form url to constants helper

--- packages/constants-helper/src/constants.ts
+++ packages/constants-helper/src/constants.ts
@@ -18,3 +18,6 @@ export const EVMOS_TOS_VERSION = "evmos-TOS-v2";
 export const ECOSYSTEM_URL =
   // eslint-disable-next-line no-secrets/no-secrets
   "https://altiplanic.notion.site/a188bd13dd114a88a7763fd2a8cc601e?v=403420ad21db41ce81f09b7e3f77e4e2";
+export const GOOGLE_FORM_URL =
+  // eslint-disable-next-line no-secrets/no-secrets
+  "https://docs.google.com/forms/d/e/1FAIpQLSc2O5HzjZMPbFD84O1513xJ0mggkSXihQy_a6IGskZq28l8yA/viewform";
