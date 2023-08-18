5253b08287a705b40ffa64bb042f538106fa71f6
Milagros Paoletti <40247040+miliPaoletti@users.noreply.github.com>
2023-08-18 10:21:12 +0200

    Update apps/mission/src/components/mission/dappStore/card/Card.tsx
    Co-authored-by: Julia Ortiz <94128293+julia-script@users.noreply.github.com>

--- apps/mission/src/components/mission/dappStore/card/Card.tsx
+++ apps/mission/src/components/mission/dappStore/card/Card.tsx
@@ -1,7 +1,21 @@
 // Copyright Tharsis Labs Ltd.(Evmos)
 // SPDX-License-Identifier:ENCL-1.0(https://github.com/evmos/apps/blob/main/LICENSE)
 
-export const Card = ({ children }: { children: JSX.Element }) => {
+import { ComponentProps } from "react";
+
+export const Card = ({
+  className,
+  ...rest
+}: ComponentProps<'section'>) => {
+  return (
+    <section 
+      className={`${className ?? ''} space-y-8 rounded-lg bg-darGray800 px-5 py-7`} 
+      {...rest}
+    />
+  );
+};
+
+
   return (
     <section className="flex flex-col justify-between space-y-8 rounded-lg bg-darGray800 p-8">
       {children}
