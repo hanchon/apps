8cff26026e300a0adbad0e8c575d5ba28c676e2e
mrsirdev <mr.sir.dev@gmail.com>
2023-08-21 12:47:43 +0100

    chore: Remove back to mission control button

--- apps/staking/src/components/staking/Content.tsx
+++ apps/staking/src/components/staking/Content.tsx
@@ -4,26 +4,15 @@
 import dynamic from "next/dynamic";
 import { SearchWrapper } from "../../internal/common/context/SearchContext";
 import { ValidatorStateWrapper } from "../../internal/common/context/ValidatorStateContext";
-import { Navigation } from "ui-helpers";
 import { tabsContent } from "./Tabs/Content";
-import { NAV_TO_MISSION_CONTROL, EVMOS_PAGE_URL } from "constants-helper";
 const TopBarStaking = dynamic(() => import("./components/TopBarStaking"));
 const Tabs = dynamic(() => import("../common/tabComponent/Tabs"));
-import { CLICK_BACK_TO_MC, useTracker } from "tracker";
 
 const Content = () => {
-  const { handlePreClickAction } = useTracker(CLICK_BACK_TO_MC);
   return (
     <SearchWrapper>
       <ValidatorStateWrapper>
         <div className="">
-          <Navigation
-            href={EVMOS_PAGE_URL}
-            text={NAV_TO_MISSION_CONTROL}
-            onClick={() => {
-              handlePreClickAction();
-            }}
-          />
           <TopBarStaking />
           <div className=" xl:scrollbar-hide mt-5 w-full px-2 font-[IBM] text-pearl">
             <Tabs
