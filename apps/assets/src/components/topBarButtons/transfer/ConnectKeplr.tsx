af114c74d783d4579a9830c354f619947c96b5cd
Milagros Paoletti <milagros.paoletti@gmail.com>
2023-08-24 12:06:15 +0200

    add wizard helper and use it in deposit and connect keplr

--- apps/assets/src/components/topBarButtons/transfer/ConnectKeplr.tsx
+++ apps/assets/src/components/topBarButtons/transfer/ConnectKeplr.tsx
@@ -2,7 +2,7 @@
 // SPDX-License-Identifier:ENCL-1.0(https://github.com/evmos/apps/blob/main/LICENSE)
 
 import { KeplrIcon } from "icons";
-import { Progress } from "ui-helpers";
+import { Progress, WizardHelper } from "ui-helpers";
 import { stepsSetAccountKeplr } from "./utils";
 import { GroupStateI } from "ui-helpers/src/progress/types";
 import { Dispatch, SetStateAction } from "react";
@@ -15,15 +15,13 @@ export const ConnectKeplr = ({
   setGroupState: Dispatch<SetStateAction<GroupStateI[]>>;
 }) => {
   return (
-    <div className="">
-      {/* TODO: create a ui-helper for this and pass the h1 and h2 for each of the cases (and the icon too) */}
-      <div className="bg-lightBlue flex space-x-2 justify-start rounded-lg p-4">
-        <KeplrIcon />
-        <div className="relative -top-[3px] text-blue text-sm">
+    <div>
+      <WizardHelper icon={<KeplrIcon />}>
+        <>
           <h1 className="font-bold">Keplr is required</h1>
           <h2>Depositing assets can only be done with the Keplr extension.</h2>
-        </div>
-      </div>
+        </>
+      </WizardHelper>
       <Progress
         steps={stepsSetAccountKeplr}
         groupState={groupState}
--- apps/assets/src/components/topBarButtons/transfer/Deposit.tsx
+++ apps/assets/src/components/topBarButtons/transfer/Deposit.tsx
@@ -1,7 +1,20 @@
 // Copyright Tharsis Labs Ltd.(Evmos)
 // SPDX-License-Identifier:ENCL-1.0(https://github.com/evmos/apps/blob/main/LICENSE)
 
+import { WizardHelper } from "ui-helpers";
+
 export const Deposit = () => {
   // TODO: add logic for deposit
-  return <></>;
+  return (
+    <section>
+      <WizardHelper>
+        <p>
+          We prefilled the address of the recipient above with{" "}
+          <b>the Evmos address that you are logged in with</b>. If you are
+          depositing into a different address, you can make the changes
+          accordingly.
+        </p>
+      </WizardHelper>
+    </section>
+  );
 };
