f3e6e3417e14835bf5a88133f85f6ccd093b78c1
Milagros Paoletti <milagros.paoletti@gmail.com>
2023-08-25 11:29:51 +0200

    use ComponentProps for input

--- packages/ui-helpers/src/modal/Input.tsx
+++ packages/ui-helpers/src/modal/Input.tsx
@@ -1,19 +1,15 @@
 // Copyright Tharsis Labs Ltd.(Evmos)
 // SPDX-License-Identifier:ENCL-1.0(https://github.com/evmos/apps/blob/main/LICENSE)
 
-type inputProps = {
-  placeholder: string;
-  value: string;
-  onChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
-  type?: string;
-};
+import { ComponentProps } from "react";
 
 export const Input = ({
   placeholder,
   value,
   onChange,
-  type = "text",
-}: inputProps) => {
+  type,
+  ...rest
+}: ComponentProps<"input">) => {
   return (
     <input
       className="w-full focus-visible:outline-none border border-gray300 rounded-lg px-3 py-2 text-sm placeholder-gray2 text-darkGray900"
@@ -21,6 +17,7 @@ export const Input = ({
       value={value}
       onChange={onChange}
       type={type}
+      {...rest}
     />
   );
 };
