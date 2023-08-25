8fcac60c885c56c1200a2240185188b10f39596f
Milagros Paoletti <milagros.paoletti@gmail.com>
2023-08-25 12:54:58 +0200

    add copyright

--- packages/ui-helpers/src/modal/Subtitle.tsx
+++ packages/ui-helpers/src/modal/Subtitle.tsx
@@ -1,3 +1,6 @@
+// Copyright Tharsis Labs Ltd.(Evmos)
+// SPDX-License-Identifier:ENCL-1.0(https://github.com/evmos/apps/blob/main/LICENSE)
+
 <h5 className="text-xs font-bold">Transfer Type</h5>;
 import { ComponentProps } from "react";
 
