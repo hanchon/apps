8a01836f29685d86b2d46aac419ee742952e9e79
Milagros Paoletti <milagros.paoletti@gmail.com>
2023-08-25 13:03:22 +0200

    fix import

--- packages/ui-helpers/src/modal/Introduction.tsx
+++ packages/ui-helpers/src/modal/Introduction.tsx
@@ -2,7 +2,9 @@
 // SPDX-License-Identifier:ENCL-1.0(https://github.com/evmos/apps/blob/main/LICENSE)
 
 import { EvmosRedIcon } from "icons";
-import { Description, Title } from "ui-helpers";
+import { Title } from "./Title";
+import { Description } from "./Description";
+
 export const IntroductionModal = ({
   title,
   description,
