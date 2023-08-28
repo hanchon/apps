5233bbaa49bdeba0b35b811036eaa25b2be111c1
Milagros Paoletti <milagros.paoletti@gmail.com>
2023-08-28 11:46:53 +0200

    display tooltip only if the user hover above the icon

--- apps/assets/src/components/topBarButtons/common/AccountInformation.tsx
+++ apps/assets/src/components/topBarButtons/common/AccountInformation.tsx
@@ -1,7 +1,9 @@
 // Copyright Tharsis Labs Ltd.(Evmos)
 // SPDX-License-Identifier:ENCL-1.0(https://github.com/evmos/apps/blob/main/LICENSE)
 
+import { Tooltip } from "ui-helpers";
 import { AddressesContainer } from "./AddressesContainer";
+import { InformationIcon } from "icons";
 
 export const AccountInformation = () => {
   return (
@@ -9,12 +11,11 @@ export const AccountInformation = () => {
       <div>
         <div className="flex items-center space-x-1">
           <h3 className="font-bold">Account information</h3>
-          {/* the hover is applied on all the height. We want it only in the icon */}
-          {/* <Tooltip
-          className="min-w-[10rem]"
-          element={<InformationIcon width={15} height={15} />}
-          text="The addresses below are the same but in different formats. We provide this information for you to quickly refer to your address in instances like depositing assets into your account."
-        /> */}
+          <Tooltip
+            className="min-w-[10rem]"
+            element={<InformationIcon width={15} height={15} />}
+            text="The addresses below are the same but in different formats. We provide this information for you to quickly refer to your address in instances like depositing assets into your account."
+          />
         </div>
       </div>
 
