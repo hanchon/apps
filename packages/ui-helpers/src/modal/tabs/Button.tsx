4b72ad41a9b95b695a506df811d961a1192b7921
Milagros Paoletti <milagros.paoletti@gmail.com>
2023-09-12 14:50:07 +0200

    update padding for Tabs pink

--- packages/ui-helpers/src/modal/tabs/Button.tsx
+++ packages/ui-helpers/src/modal/tabs/Button.tsx
@@ -17,13 +17,13 @@ export const Button = ({
     <button
       onClick={tab.onClick}
       className={cx(
-        "flex items-center justify-center gap-1 rounded-md px-3 py-1.5",
+        "flex items-center justify-center gap-1 rounded-md py-1 px-3",
         {
           "border-red border-2":
             variant === "default" && tab.option === tab.type,
           "border-strokeGrey border":
             variant === "default" && tab.option !== tab.type,
-          "border-[#FFA1A1] border-2 bg-[#FF9E90] shadow-custom-pink text-black font-medium":
+          "border-[#FFA1A1] border-2 bg-[#FF9E90] shadow-custom-pink text-black font-medium ":
             variant === "pink" && tab.option === tab.type,
           "bg-pink-200 text-black font-light":
             variant === "pink" && tab.option !== tab.type,
