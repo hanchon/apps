569ffb98142d35b9e57cd47b8c4d83a30d83df56
Julia Ortiz <contato@juliaortiz.com.br>
2023-09-16 17:18:41 -0300

    simplify styling on modalWithTransition a bit and fixes the problem of not closing the modal when clicking on the courtain

--- packages/ui-helpers/src/ModalWithTransitions.tsx
+++ packages/ui-helpers/src/ModalWithTransitions.tsx
@@ -55,63 +55,56 @@ export const ModalWithTransitions = ({
           leaveFrom="transform scale-100 opacity-100"
           leaveTo="transform scale-95 opacity-0"
         >
-          <div className="fixed inset-0 bg-black bg-opacity-50" />
+          <div
+            className="fixed inset-0 bg-black bg-opacity-50"
+            onClick={() => {
+              console.log("click");
+              setShow(false);
+            }}
+          />
         </Transition.Child>
 
-        <div
-          className="fixed inset-0 z-10 overflow-y-auto pb-24 pt-8"
-          onClick={() => {
-            setShow(false);
-          }}
-        >
-          <div
-            className="flex min-h-full items-center justify-center sm:p-4 text-center p-0"
-            onClick={(e) => {
-              e.stopPropagation();
-            }}
+        <div className="fixed inset-0 pointer-events-none h-full w-full flex items-center justify-center py-4">
+          <Transition.Child
+            as={Fragment}
+            enter="transition duration-100 ease-in"
+            enterFrom="transform scale-95 opacity-0"
+            enterTo="transform scale-100 opacity-100"
+            leave="transition duration-100 ease-out"
+            leaveFrom="transform scale-100 opacity-100"
+            leaveTo="transform scale-95 opacity-0"
           >
-            <Transition.Child
-              as={Fragment}
-              enter="transition duration-100 ease-in"
-              enterFrom="transform scale-95 opacity-0"
-              enterTo="transform scale-100 opacity-100"
-              leave="transition duration-100 ease-out"
-              leaveFrom="transform scale-100 opacity-100"
-              leaveTo="transform scale-95 opacity-0"
+            <Dialog.Panel
+              className={cx(
+                "relative transform overflow-auto max-h-full rounded-lg text-left transition-all  pointer-events-auto",
+                {
+                  "bg-pearl1 shadow-xl md:min-w-[400px] max-w-[850px]":
+                    variant === "default",
+                  "bg-black-900 shadow-custom-sm px-6 pt-6 pb-16 text-white w-full max-w-md":
+                    variant === "modal-black",
+                }
+              )}
             >
-              <Dialog.Panel
-                className={cx(
-                  "relative transform overflow-hidden rounded-lg text-left transition-all",
-                  {
-                    "bg-pearl1 shadow-xl md:min-w-[400px] max-w-[850px]":
-                      variant === "default",
-                    "bg-black-900 shadow-custom-sm px-6 pt-6 pb-16 text-white w-full max-w-md":
-                      variant === "modal-black",
-                  }
-                )}
-              >
-                <div className="absolute right-0 top-0 block pr-4 pt-4">
-                  {propClose && (
-                    <button
-                      type="button"
-                      className="focus-visible:outline-none"
-                      onClick={handleCloseModal}
-                    >
-                      <span className="sr-only">Close</span>
-                      <CloseIcon
-                        className={cx("h-6 w-auto", {
-                          "text-gray2": variant === "default",
-                          "text-pink-300": variant === "modal-black",
-                        })}
-                        aria-hidden="true"
-                      />
-                    </button>
-                  )}
-                </div>
-                {children}
-              </Dialog.Panel>
-            </Transition.Child>
-          </div>
+              {propClose && (
+                <button
+                  type="button"
+                  className="focus-visible:outline-none absolute right-6 top-4"
+                  onClick={handleCloseModal}
+                >
+                  <span className="sr-only">Close</span>
+                  <CloseIcon
+                    className={cx("h-6 w-auto", {
+                      "text-gray2": variant === "default",
+                      "text-pink-300": variant === "modal-black",
+                    })}
+                    aria-hidden="true"
+                  />
+                </button>
+              )}
+
+              {children}
+            </Dialog.Panel>
+          </Transition.Child>
         </div>
       </Dialog>
     </Transition>
