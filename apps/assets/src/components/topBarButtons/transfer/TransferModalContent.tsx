e4a426902688fce3fb6046270c0bb299922207d8
Julia Ortiz <contato@juliaortiz.com.br>
2023-09-25 09:45:06 -0300

    fix 'pay' cta flashing before redirecting to receipt modal

--- apps/assets/src/components/topBarButtons/transfer/TransferModalContent.tsx
+++ apps/assets/src/components/topBarButtons/transfer/TransferModalContent.tsx
@@ -433,7 +433,7 @@ export const TransferModalContent = ({
                 }
               >
 
-                {isTransferring ? <><Spinner /> {t("transfer.send.button.processing.text")}</> : t("transfer.send.button.text")}
+                {isTransferring || hasTransferred ? <><Spinner /> {t("transfer.send.button.processing.text")}</> : t("transfer.send.button.text")}
               </PrimaryButton>
 
             )}
