fa84e5a3a0f0c07177e32d8812e588d132bedfcb
Julia Ortiz <contato@juliaortiz.com.br>
2023-09-25 12:16:07 -0300

    address height issue with modals

--- packages/ui-helpers/src/ModalWithTransitions.tsx
+++ packages/ui-helpers/src/ModalWithTransitions.tsx
@@ -63,7 +63,7 @@ export const ModalWithTransitions = ({
           />
         </Transition.Child>
 
-        <div className="fixed inset-0 pointer-events-none h-full w-full flex items-center justify-center py-4">
+        <div className="fixed inset-0 pointer-events-none h-full w-full flex  py-4 overflow-y-auto">
           <Transition.Child
             as={Fragment}
             enter="transition duration-100 ease-in"
@@ -75,7 +75,7 @@ export const ModalWithTransitions = ({
           >
             <Dialog.Panel
               className={cx(
-                "relative transform max-h-full rounded-lg text-left transition-all pointer-events-auto",
+                "relative transform rounded-lg text-left m-auto transition-all pointer-events-auto",
                 {
                   "bg-pearl1 shadow-xl md:min-w-[400px] max-w-[850px]":
                     variant === "default",
