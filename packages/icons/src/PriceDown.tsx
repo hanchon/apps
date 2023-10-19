9a11c0afe19d1b9454250db2c4693a1689480a2d
Milagros Paoletti <milagros.paoletti@gmail.com>
2023-10-19 09:51:37 +0200

    fix svg

--- packages/icons/src/PriceDown.tsx
+++ packages/icons/src/PriceDown.tsx
@@ -11,11 +11,28 @@ export const PriceDown: React.FC<PriceDownIconProps> = ({
   ...restProps
 }) => {
   return (
-    <svg {...restProps} width={width} height={height} viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
-      <path d="M1 6.25L8 13.25L10.625 10.625L15 15" stroke="#ED4E33" stroke-width="1.2" stroke-linecap="round" stroke-linejoin="round" />
-      <path d="M11.5 1V8M11.5 8L14.125 5.375M11.5 8L8.875 5.375" stroke="#ED4E33" stroke-width="1.2" stroke-linecap="round" stroke-linejoin="round" />
+    <svg
+      {...restProps}
+      width={width}
+      height={height}
+      viewBox="0 0 16 16"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M1 6.25L8 13.25L10.625 10.625L15 15"
+        stroke="#ED4E33"
+        strokeWidth="1.2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M11.5 1V8M11.5 8L14.125 5.375M11.5 8L8.875 5.375"
+        stroke="#ED4E33"
+        strokeWidth="1.2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
     </svg>
-
   );
 };
-
