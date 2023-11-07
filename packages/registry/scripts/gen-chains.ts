5c0c7fecd882f6d2556228dc192cedff8efca666
Milagros Paoletti <milagros.paoletti@gmail.com>
2023-11-07 17:17:09 +0100

    update img for kujira. Show kujira network for assets

--- packages/registry/scripts/gen-chains.ts
+++ packages/registry/scripts/gen-chains.ts
@@ -84,30 +84,31 @@ for (const chainRegistry of chains) {
 
   const identifier = normalizeIdentifier(configuration);
 
-  const tokens = tokenByIdentifier[identifier]?.map((token) => {
-    return {
-      name: token.name,
-      ref: `${chainRegistry.prefix}:${token.coinDenom}`,
-      description: token.description,
-      symbol: token.coinDenom,
-      denom: token.coinDenom,
-      sourcePrefix: chainRegistry.prefix,
-      sourceDenom:
-        chainRegistry.prefix === "evmos"
-          ? token.cosmosDenom
-          : token.ibc.sourceDenom,
-      // TODO: minCoinDenom for evmos is wrong in our registry, we should fix that there
-      minCoinDenom:
-        token.minCoinDenom === "EVMOS" ? "aevmos" : token.minCoinDenom,
-      category: token.category === "none" ? null : token.category,
-      tokenRepresentation: token.tokenRepresentation as string | null,
-      type: token.type === "IBC" ? "IBC" : "ERC20",
-      decimals: Number(token.exponent),
-      erc20Address: token.erc20Address as string | null,
-      handledByExternalUI: token.handledByExternalUI ?? null,
-      listed: true,
-    };
-  });
+  const tokens =
+    tokenByIdentifier[identifier]?.map((token) => {
+      return {
+        name: token.name,
+        ref: `${chainRegistry.prefix}:${token.coinDenom}`,
+        description: token.description,
+        symbol: token.coinDenom,
+        denom: token.coinDenom,
+        sourcePrefix: chainRegistry.prefix,
+        sourceDenom:
+          chainRegistry.prefix === "evmos"
+            ? token.cosmosDenom
+            : token.ibc.sourceDenom,
+        // TODO: minCoinDenom for evmos is wrong in our registry, we should fix that there
+        minCoinDenom:
+          token.minCoinDenom === "EVMOS" ? "aevmos" : token.minCoinDenom,
+        category: token.category === "none" ? null : token.category,
+        tokenRepresentation: token.tokenRepresentation as string | null,
+        type: token.type === "IBC" ? "IBC" : "ERC20",
+        decimals: Number(token.exponent),
+        erc20Address: token.erc20Address as string | null,
+        handledByExternalUI: token.handledByExternalUI ?? null,
+        listed: true,
+      };
+    }) ?? [];
 
   const isTestnet = configuration.configurationType === "testnet";
   const feeTokenFromChainConfig = configuration.currencies[0];
