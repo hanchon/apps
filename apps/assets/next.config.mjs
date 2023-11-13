6a0f54b6e7a2a3710a805518ce1bed315e27a4a3
Julia Ortiz <contato@juliaortiz.com.br>
2023-11-13 15:59:18 -0300

    removes deprecated next apps

--- apps/assets/next.config.mjs
+++
@@ -1,9 +0,0 @@
-// Copyright Tharsis Labs Ltd.(Evmos)
-// SPDX-License-Identifier:ENCL-1.0(https://github.com/evmos/apps/blob/main/LICENSE)
-
-import { withEvmosConfig } from "@evmosapps/config/next/with-config.js";
-import locale from "./next-i18next.config.js";
-export default withEvmosConfig({
-  basePath: "/portfolio",
-  i18n: locale.i18n,
-});
--- apps/mission/setup-redirects.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-if [ "$NEXT_PUBLIC_EVMOS_APP_ENV" = "production" ]; then
-  cp public/_redirects_prod public/_redirects
-else
-  cp public/_redirects_staging public/_redirects
-fi
