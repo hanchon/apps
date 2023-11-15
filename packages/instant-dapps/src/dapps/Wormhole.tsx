cba9170d0361d982fbe8311e8fdfbee6a5e5f74e
Milagros Paoletti <milagros.paoletti@gmail.com>
2023-11-15 15:08:48 +0100

    add wormhole on instant-dapp

---
+++ packages/instant-dapps/src/dapps/Wormhole.tsx
@@ -0,0 +1,25 @@
+"use client";
+// Copyright Tharsis Labs Ltd.(Evmos)
+// SPDX-License-Identifier:ENCL-1.0(https://github.com/evmos/apps/blob/main/LICENSE)
+
+import WormholeBridge, {
+  WormholeConnectConfig,
+} from "@wormhole-foundation/wormhole-connect";
+
+export default function Wormhole() {
+  const config: WormholeConnectConfig = {
+    env: "mainnet",
+    bridgeDefaults: {
+      fromNetwork: "ethereum",
+      toNetwork: "evmos",
+      token: "USDCeth",
+      requiredNetwork: "evmos",
+    },
+  };
+
+  return (
+    <div className="-mt-8 w-full">
+      <WormholeBridge config={config} />
+    </div>
+  );
+}
