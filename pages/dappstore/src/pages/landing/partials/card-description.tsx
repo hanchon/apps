9303d7a59dd7026082da4310b3f507bee6428871
Milagros Paoletti <milagros.paoletti@gmail.com>
2023-11-16 16:57:16 +0100

    remove text

--- pages/dappstore/src/pages/landing/partials/card-description.tsx
+++ pages/dappstore/src/pages/landing/partials/card-description.tsx
@@ -2,5 +2,5 @@
 // SPDX-License-Identifier:ENCL-1.0(https://github.com/evmos/apps/blob/main/LICENSE)
 
 export const CardDescription = ({ text }: { text: string }) => {
-  return <h2 className="font-medium text-white opacity-70">dsds{text}</h2>;
+  return <h2 className="font-medium text-white opacity-70">{text}</h2>;
 };
