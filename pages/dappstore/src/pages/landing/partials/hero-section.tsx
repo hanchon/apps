228ee47529b83ddbf465aa8e85e41afc5939019a
Milagros Paoletti <milagros.paoletti@gmail.com>
2023-12-14 10:22:24 +0100

    update for hero section

--- pages/dappstore/src/pages/landing/partials/hero-section.tsx
+++ pages/dappstore/src/pages/landing/partials/hero-section.tsx
@@ -17,37 +17,37 @@ export const HeroSection = async ({ totalApps }: { totalApps: number }) => {
   const { t } = await translation("dappStore");
   return (
     <div className="grid grid-cols-1 md:grid-cols-2 space-x-0 md:space-x-8">
-      <Frameline>
-        <div className="w-full flex flex-col space-y-8 py-5 px-0 md:px-[6.2rem] ">
-          <div className="flex flex-col font-evmos font-light text-2xl md:text-[2.5em] tracking-wide text-pearl leading-none space-y-2 ">
+      <Frameline variant="secondary">
+        <div className="w-full flex flex-col space-y-8 py-5 px-0 lg:px-[4.2rem] xl-px-[5rem]">
+          <div className="flex flex-col font-evmos font-light text-2xl lg:text-[2.5em] tracking-wide text-pearl leading-none space-y-2 ">
             <h1
-              className="text-justify after:content-[''] -mb-5 md:-mb-10 after:inline-block after:w-full"
+              className="text-justify after:content-[''] -mb-5 sm:-mb-5 lg:-mb-10 after:inline-block after:w-full"
               style={{ wordSpacing: "-11px" }}
             >
               {t("ecosystem.title")}
             </h1>
             <div
               className="flex items-center justify-between relative before:content-[''] before:absolute before:top-[50%] before:left-0 
-        before:-translate-y-1/2 before:w-[20px] before:h-[20px] before:animate-pulse before:bg-[#9F05FE] before:rounded-full"
+        before:-translate-y-1/2 before:w-[15px] before:h-[15px] before:md:w-[20px] before:md:h-[20px] before:animate-pulse before:bg-[#9F05FE] before:rounded-full"
             >
-              <div className="pl-10 flex items-center space-x-6">
+              <div className="pl-10 flex items-center space-x-6 md:space-x-10 ">
                 <UpRightArrowIcon
                   width={20}
                   height={20}
                   strokeWidth={4}
-                  className="text-[#A4A189]"
+                  className="text-[#A4A189] w-4 h-4 md:w-5 md:h-5"
                 />
                 <UpRightArrowIcon
                   width={20}
                   height={20}
                   strokeWidth={4}
-                  className="text-[#A4A189]"
+                  className="text-[#A4A189] w-4 h-4 md:w-5 md:h-5"
                 />
                 <UpRightArrowIcon
                   width={20}
                   height={20}
                   strokeWidth={4}
-                  className="text-[#A4A189]"
+                  className="text-[#A4A189] w-4 h-4 md:w-5 md:h-5"
                 />
               </div>
               <span
@@ -63,7 +63,7 @@ export const HeroSection = async ({ totalApps }: { totalApps: number }) => {
           </p>
           <div className="flex flex-col md:flex-row items-center space-x-0 space-y-4 md:space-y-0 md:space-x-2 w-full">
             <PrimaryButton
-              className="flex-1 font-normal rounded w-full self-stretch"
+              className="flex-1 font-normal rounded w-full self-stretch text-center"
               as={"a"}
               href={ADD_DAPP_FORM_URL}
               target="_blank"
