27ff824c5bcd860474dd1e50b242557d93a3696d
Milagros Paoletti <milagros.paoletti@gmail.com>
2023-12-20 10:08:31 +0100

    sort related apps

--- pages/dappstore/src/pages/dapp-explorer/dapp-details/dapp-details-page.tsx
+++ pages/dappstore/src/pages/dapp-explorer/dapp-details/dapp-details-page.tsx
@@ -24,6 +24,16 @@ export const DappDetailsPage = async ({
       (dApp) =>
         dApp.categoryName === dapp?.categoryName && dApp.slug !== dapp?.slug
     )
+    .sort((a, b) => {
+      const alphabeticalOrder = a.name.localeCompare(b.name);
+
+      // If both apps have the same instant-dapp property, return alphabetical order
+      if (a.instantDapp === b.instantDapp) {
+        return alphabeticalOrder;
+      }
+      // Otherwise, instant-dapp apps come first
+      return a.instantDapp ? -1 : 1;
+    })
     .slice(0, 4);
 
   if (!dapp) {
