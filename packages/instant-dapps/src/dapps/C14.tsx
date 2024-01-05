0db0a494557465aeba5ccb2e1205dfece4f95519
Milagros Paoletti <milagros.paoletti@gmail.com>
2024-01-05 17:27:37 +0100

    fix typo

--- packages/instant-dapps/src/dapps/C14.tsx
+++ packages/instant-dapps/src/dapps/C14.tsx
@@ -3,7 +3,7 @@
 // Copyright Tharsis Labs Ltd.(Evmos)
 // SPDX-License-Identifier:ENCL-1.0(https://github.com/evmos/apps/blob/main/LICENSE)
 
-import C14 from "@evmosapps/widgets/src/C14";
+import C14 from "@evmosapps/widgets/src/c14";
 import { InstantDappContainer } from "./instant-dapp-container";
 
 export default function C14InstantDapp() {
