a3c8c9d7ed7d0cb16527a88ce47e949744cda6ea
Julia Ortiz <contato@juliaortiz.com.br>
2024-01-04 23:39:29 -0300

    test

--- packages/evmos-wallet/src/server/fetch-token-price-by-denom.ts
+++ packages/evmos-wallet/src/server/fetch-token-price-by-denom.ts
@@ -1,23 +1,28 @@
+"use server";
 import { TokenSymbol } from "@evmosapps/registry/autogen/registry";
 import { fetchTokenPrices } from "./fetch-token-prices";
 import { raise } from "helpers/src/error-handling/assertions";
+import { unstable_cache } from "next/cache";
+export const fetchTokenPriceByDenom = unstable_cache(
+  async (denom: TokenSymbol | (string & {})) => {
+    return (
+      (await fetchTokenPrices().then((prices) =>
+        Object.values(prices).find(({ coinDenom }) => {
+          return coinDenom.toLowerCase() === denom.toLowerCase();
+        })
+      )) ?? raise("Token price not found")
+    );
+  },
+  ["fetchTokenPriceByDenom"],
+  {
+    revalidate: 60,
+  }
+);
 
-export const fetchTokenPriceByDenom = async (
-  denom: TokenSymbol | (string & {})
-) => {
-  return (
-    (await fetchTokenPrices().then((prices) =>
-      Object.values(prices).find(({ coinDenom }) => {
-        return coinDenom.toLowerCase() === denom.toLowerCase();
-      })
-    )) ?? raise("Token price not found")
-  );
-};
-
-export const apiFetchTokenPriceByDenom = async (
-  denom: TokenSymbol | (string & {})
-) => {
-  return fetch(`/api/actions/token-price-by-denom/${denom.toLowerCase()}`).then(
-    (res) => res.json()
-  ) as ReturnType<typeof fetchTokenPriceByDenom>;
-};
+// export const apiFetchTokenPriceByDenom = async (
+//   denom: TokenSymbol | (string & {})
+// ) => {
+//   return fetch(`/api/actions/token-price-by-denom/${denom.toLowerCase()}`).then(
+//     (res) => res.json()
+//   ) as ReturnType<typeof fetchTokenPriceByDenom>;
+// };
