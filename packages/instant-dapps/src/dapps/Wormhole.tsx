c5e326ad5fd95f85797052e484a45b8879c1c048
Milagros Paoletti <milagros.paoletti@gmail.com>
2024-01-09 16:26:31 +0100

    add blur to wormhole

--- packages/instant-dapps/src/dapps/Wormhole.tsx
+++ packages/instant-dapps/src/dapps/Wormhole.tsx
@@ -3,7 +3,14 @@
 // SPDX-License-Identifier:ENCL-1.0(https://github.com/evmos/apps/blob/main/LICENSE)
 
 import Wormhole from "@evmosapps/widgets/src/wormhole";
+import { InstantDappContainer } from "./instant-dapp-container";
 
 export default function WormholeInstantDapp() {
-  return <Wormhole />;
+  return (
+    <InstantDappContainer
+      image="bg-[url(/ecosystem/blur/wormhole-blur.png)]"
+      dappName="Wormhole"
+      widget={<Wormhole />}
+    />
+  );
 }
