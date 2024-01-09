d130993e04f3309abf85c337bd8b9cdd7f033e23
Milagros Paoletti <milagros.paoletti@gmail.com>
2024-01-09 17:40:40 +0100

    add banner component

---
+++ packages/stateful-components/src/banner.tsx
@@ -0,0 +1,10 @@
+import { Banner } from "@evmosapps/ui-helpers";
+
+export const StatefulBanner = () => {
+  // TODO: add notion variables
+  return (
+    <Banner>
+      We are experiencing some problems! Sorry for the inconvenience!
+    </Banner>
+  );
+};
