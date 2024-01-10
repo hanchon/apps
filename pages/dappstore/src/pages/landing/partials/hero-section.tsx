1bcb3a8ab1a06b2d39b1fa63f544dc9141d65271
Milagros Paoletti <milagros.paoletti@gmail.com>
2024-01-10 17:22:06 +0100

    fix space between hero sections

--- pages/dappstore/src/pages/landing/partials/hero-section.tsx
+++ pages/dappstore/src/pages/landing/partials/hero-section.tsx
@@ -5,7 +5,7 @@ import { HeroSectionFrameline } from "./hero-section-frameline";
 
 export const HeroSection = ({ totalApps }: { totalApps: number }) => {
   return (
-    <div className="grid grid-cols-1 md:grid-cols-2 space-x-0 md:space-x-8">
+    <div className="grid md:grid-cols-2 gap-x-8 ">
       <HeroSectionFrameline />
       <div className="hidden md:grid">
         <HeroSectionExplore totalApps={totalApps} />
