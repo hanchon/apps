a3a1831f7388559c39d46ce8bd78c7091f298936
Milagros Paoletti <milagros.paoletti@gmail.com>
2024-01-15 10:45:32 +0100

    switch hero section components

--- pages/dappstore/src/pages/landing/partials/hero-section.tsx
+++ pages/dappstore/src/pages/landing/partials/hero-section.tsx
@@ -5,11 +5,11 @@ import { HeroSectionFrameline } from "./hero-section-frameline";
 
 export const HeroSection = ({ totalApps }: { totalApps: number }) => {
   return (
-    <div className="grid md:grid-cols-2 gap-x-8 ">
-      <HeroSectionFrameline />
-      <div className="hidden md:grid">
+    <div className="grid md:grid-cols-2 gap-x-8">
+      <div className="hidden md:grid ">
         <HeroSectionExplore totalApps={totalApps} />
       </div>
+      <HeroSectionFrameline />
     </div>
   );
 };
