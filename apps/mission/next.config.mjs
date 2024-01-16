4898bc636fa6f5667b28ff3bb630f51f9cf3400e
Julia Ortiz <contato@juliaortiz.com.br>
2024-01-16 14:20:42 -0300

    Sets vercel redirects

--- apps/mission/next.config.mjs
+++ apps/mission/next.config.mjs
@@ -3,6 +3,18 @@
 
 import { withEvmosConfig } from "@evmosapps/config/next/with-config.js";
 import locale from "./next-i18next.config.js";
+
 export default withEvmosConfig({
   i18n: locale.i18n,
+
+  async rewrites() {
+    if (process.env.VERCEL !== "1") {
+      return [];
+    }
+    const suffix = process.env.VERCEL_ENV === "production" ? "" : "-staging";
+    return ["portfolio", "staking", "governance"].map((app) => ({
+      source: `/${app}/:path*`,
+      destination: `https://evmos-${app + suffix}.vercel.app/portfolio/:path*`,
+    }));
+  },
 });
