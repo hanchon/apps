36a50ef1be7e4c84a177cff8d6e6d16995156ad6
Julia Ortiz <contato@juliaortiz.com.br>
2024-01-22 11:36:43 -0300

    adds staging url as fallback

--- packages/helpers/src/clients/get-pub-url.ts
+++ packages/helpers/src/clients/get-pub-url.ts
@@ -1,6 +1,17 @@
-export const getPubUrl = () =>
-  process.env.VERCEL_URL ||
-  process.env.NEXT_PUBLIC_VERCEL_URL ||
-  (typeof window === "undefined" && process.env.PORT)
-    ? `http://localhost:${process.env.PORT}`
-    : "";
+export const getPubUrl = () => {
+  if (typeof window === "undefined") {
+    if (process.env.NODE_ENV === "development") {
+      return (
+        process.env.VERCEL_URL ||
+        process.env.NEXT_PUBLIC_VERCEL_URL ||
+        `http://localhost:${process.env.PORT}`
+      );
+    }
+    return (
+      process.env.VERCEL_URL ||
+      process.env.NEXT_PUBLIC_VERCEL_URL ||
+      "https://staging-app.evmos.org"
+    );
+  }
+  return "";
+};
